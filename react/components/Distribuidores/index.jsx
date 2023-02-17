4a4db31425f6f3fae4b836ee6d849a386ced627a
luccas-baptista <luccas.fill@gmail.com>
2023-02-17 10:34:56 -0300

    alteracoes distribuidores

--- react/components/Distribuidores/index.jsx
+++ react/components/Distribuidores/index.jsx
@@ -3,10 +3,14 @@ import React, { useEffect, useState } from "react";
 import "./distribuidores.css";
 
 const Distribuidores = () => {
+
   const [id, setId] = React.useState([]);
+
   React.useEffect(() => {
-    // getAllUser();    getUser();
+    // getAllUser();
+    getUser();
   }, []);
+
   const getUser = async () => {
     const api_response = await fetch(
       "https://distribuidores.choyer.com.br/distribuidores.json",
@@ -14,53 +18,53 @@ const Distribuidores = () => {
         method: "GET",
         headers: {
           "Content-Type": "application/json"
-         }
+        }
       }
     );
     const my_user = await api_response.json();
     console.log(my_user);
     setId(my_user);
   };
+
   return (
     <>
-    <div className="main-distribuidores">
+      <div className="main-distribuidores">
 
-     <div className="main-title-distribuidores">
-      <h1>Nossos Distribuidores</h1>
-      </div>
+        <div className="main-title-distribuidores">
+          <h1>Nossos Distribuidores</h1>
+        </div>
+
         {/* MAP */}
         {id.map((estados) => (
-
         <ul key="estados.id" className="estado-wrapper">
-
-        <li className="estado-title">
-        <h2>{estados.estado}</h2>
-        </li>
-        <div className="estado-lojas">
-        <ul>
-           {estados.pontos.map(ponto => (
+          <li className="estado-title">
+            <h2>{estados.estado}</h2>
+          </li>
+          <div className="estado-lojas">
+            <ul>
+            {estados.pontos.map(ponto => (
               <li key="ponto" className="card-loja">
-                  <div className="cidade-loja">
-                    <h3>{ponto.cidade}</h3>
-                  </div>
-                  <div className="nome-loja">
-                    <h4>{ponto.nome}</h4>
-                  </div>
-                  <div className="endereco-loja">
-                    <p>{ponto.end}</p>
-                  </div>
-                  <div className="telefone-loja">
-                    <p>{ponto.tel}</p>
-                  </div>
+                <div className="cidade-loja">
+                  <h3>{ponto.cidade}</h3>
+                </div>
+                <div className="nome-loja">
+                  <h4>{ponto.nome}</h4>
+                </div>
+                <div className="endereco-loja">
+                  <p>{ponto.end}</p>
+                </div>
+                <div className="telefone-loja">
+                  <p>{ponto.tel}</p>
+                </div>
               </li>
-                 ))}
+              ))}
             </ul>
-        </div>
+          </div>
         </ul>
-             ))}
+         ))}
       </div>
-      </>
-      )
+    </>
+  )
 }
 
 export default Distribuidores
